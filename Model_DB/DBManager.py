6f47feecc2c9886c55a9ce570e09e8ee6f8a67b5
ZeroKong413 <asdfzxcv5187@gmail.com>
2024-07-16 09:51:11 +0900

    0716

--- Model_DB/DBManager.py
+++ Model_DB/DBManager.py
@@ -1,20 +1,34 @@
 #db
 # 1. db를 2개로 나눠서 관리?
 # 2. 그냥 MongoDB에 싹 다 넣기
+# gridfs를 쓸만큼 데이터의 크기가 클까?
 import base64
 from datetime import datetime
 from pymongo import MongoClient
+import json
+import os
+import gridfs
 
-# MongoDB 클라이언트 설정
+# MongoDB 연결
 client = MongoClient('localhost', 27017)
 db = client['robotData']
 collection = db['Data']
+fs = gridfs.GridFS(db)
 
-# 이미지 데이터를 base64로 인코딩
-def encode_image(image_path):
-    with open(image_path, "rb") as image_file:
-        return base64.b64encode(image_file.read()).decode('utf-8')
+json_file = 'json파일의 경로'
 
+image_path = 'image파일의 경로'
 
+with open(json_file, 'r') as file:
+    data = json.load(file)
 
+for item in data:
+    image_path = os.path.join(image_path, item['imageData'])
+    with open(image_path, 'rb') as f:
+        image_id = fs.put(f, filename=item['imageData'])
+    item['imageData'] = image_id
+
+collection.insert_many(data)
+
+print("save data")
 
